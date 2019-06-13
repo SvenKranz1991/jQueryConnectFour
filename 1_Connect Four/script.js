1dcd5b2c3f169fe289f561f5bf29d5f62e2f56a7
Sven Kranz <svenkranz@t-online.de>
2019-06-13 23:20:21 +0200

    Exercises for ThursdayWk3, Incremental Search Problems

---
+++ 1_Connect Four/script.js
@@ -0,0 +1,1 @@
+// 1. Write a function that expects a string representing a selector to be passed as a parameter. The function should find all the elements in the document that match the selector and change their style so that the text they contain is italic, underlined, and bold.
